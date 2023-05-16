30410743a536e2cecad936d53e871b07fa3a5317
MRizalSyahputra <muhammad0367ft.2022@student.uny.ac.id>
2023-05-16 19:13:35 +0700

    waduh

--- modul11/koneksi.php
+++ modul11/koneksi.php
@@ -1,3 +1,3 @@
 <?php
-    $db_con = mysqli_connect('localhost','root', 'db_html')
-    or die ('koneksi gagal');
+$db_con = mysqli_connect('localhost', 'root', '', 'db_pemroweb')
+    or die('koneksi gagal');
