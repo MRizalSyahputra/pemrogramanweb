e85d1bfca33937dbafceffa766e3995000c653bc
MRizalSyahputra <muhammad0367ft.2022@student.uny.ac.id>
2023-06-03 15:20:48 +0700

    akhir semester...

--- Uas/function.php
+++ Uas/function.php
@@ -1,1 +1,15 @@
-!
+<php 
+    //menghubungkan situs dengan database
+    $conn=mysqli_connect("localhost", "root" , "" , "pemroweb" );
+    
+    function query ($query) { 
+        global $conn; //mengambil variabel conn dari luar function
+        
+        $result=mysqli_query($conn, $query); 
+        $rows=[]; 
+        
+        while( $row=mysqli_fetch_assoc($result) ) { 
+            $ rows []=$row;
+        } return $rows; 
+    } 
+?>
